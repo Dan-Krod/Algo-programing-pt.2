122a83475c4a9834b5f3f2e34230002c51e8cc7b
Dan Krod <dan.ga17da@gmail.com>
2024-05-02 21:11:36 +0300

    Divided into two functions

--- src/diameter_of_binary_tree.py
+++ src/diameter_of_binary_tree.py
@@ -10,23 +10,25 @@ def max_diameter_of_binary_tree(tree):
         return 0
 
     max_diameter = [0]
-
-    def diameter_and_len_depth_of_binary_tree(tree):
-        if not tree:
-            return 0
-        else:
-            len_left_depth = diameter_and_len_depth_of_binary_tree(tree.left)
-            len_right_depth = diameter_and_len_depth_of_binary_tree(tree.right)
-
-            cur_diameter = len_right_depth + len_left_depth 
-            if max_diameter[0] < cur_diameter:
-                max_diameter[0] = cur_diameter
-
-            if len_left_depth > len_right_depth:
-                return len_left_depth + 1
-            else:
-                return len_right_depth + 1
-
-    diameter_and_len_depth_of_binary_tree(tree)
+    
+    diameter_and_len_depth_of_binary_tree(tree, max_diameter)
     return max_diameter[0]
+
+def diameter_and_len_depth_of_binary_tree(tree, max_diameter):
+    if not tree:
+         return 0
+    else:
+        len_left_depth = diameter_and_len_depth_of_binary_tree(tree.left, max_diameter)
+        len_right_depth = diameter_and_len_depth_of_binary_tree(tree.right, max_diameter)
+
+        cur_diameter = len_right_depth + len_left_depth 
+        if max_diameter[0] < cur_diameter:
+            max_diameter[0] = cur_diameter
+
+        if len_left_depth > len_right_depth:
+            return len_left_depth + 1
+        else:
+            return len_right_depth + 1
+
+    
   
