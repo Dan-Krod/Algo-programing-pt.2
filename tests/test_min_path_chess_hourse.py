279df47ebf7a35b4fc9b95051da3292d6de0eccc
Dan Krod <dan.ga17da@gmail.com>
2024-05-16 22:25:38 +0300

    Corrected ways in methods

--- tests/test_min_path_chess_hourse.py
+++ tests/test_min_path_chess_hourse.py
@@ -10,8 +10,8 @@ from src.min_path_chess_hourse import get_min_distance_of_chess_horse
 class TestChessPath(unittest.TestCase):
 
     def test_files_with_value(self):
-        file_input = "resources/test_input.txt"
-        file_output = "resources/test_output.txt"
+        file_input = "resources/min_path_horse_input.txt"
+        file_output = "resources/min_path_horse_output.txt"
         get_min_distance_of_chess_horse(file_input, file_output)
         with open(file_output, "r") as file:
             result = int(file.read())
@@ -19,8 +19,8 @@ class TestChessPath(unittest.TestCase):
         self.assertEqual(result, 6)
 
     def test_empty_files(self):
-        empty_file_input = "resources/test_empty_input.txt"
-        empty_file_output = "resources/test_empty_output.txt"
+        empty_file_input = "resources/min_path_horse_empty_input.txt"
+        empty_file_output = "resources/min_path_horse_empty_output.txt"
         get_min_distance_of_chess_horse(empty_file_input, empty_file_output)
         with open(empty_file_output, "r") as file:
             result = int(file.read())
@@ -28,8 +28,8 @@ class TestChessPath(unittest.TestCase):
         self.assertEqual(result, -1)
 
     def test_wrong_files(self):
-        wrong_file_input = "resources/test_wrong_input.txt"
-        wrong_file_output = "resources/test_wrong_output.txt"
+        wrong_file_input = "resources/min_path_horse_wrong_input.txt"
+        wrong_file_output = "resources/min_path_horse_wrong_output.txt"
         get_min_distance_of_chess_horse(wrong_file_input, wrong_file_output)
         with open(wrong_file_output, "r") as file:
             result = int(file.read())
